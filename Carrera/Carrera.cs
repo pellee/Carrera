785fd8af08547e2cfc5f94920b9a1f73361cb047
Pelle <joaquinpellegrino@gmail.com>
2018-04-24 13:51:22 -0300

    Creo los metodos para crear equipo, corredor y etapa

--- Carrera/Carrera.cs
+++ Carrera/Carrera.cs
@@ -13,7 +13,83 @@ namespace Carrera
         Etapa[] etapas = new Etapa[10];
 
 
+        public void CrearEquipo()
+        {
+            var equipo = new Equipo();
 
+            Console.WriteLine("Ingrese el nombre del equipo; ");
+            equipo.NombreEqui = Console.ReadLine();
+
+            equipo.Id = equipos.Count + 1;
+
+            equipos.Add(equipo);
+        }
+
+        private Equipo BuscarEquipo(string nombreEqui)
+        {
+            foreach (var e in equipos) {
+                if (nombreEqui == e.NombreEqui)
+                    return e;
+            }
+
+            return null;
+        }
+
+        public void CrearCorredor()
+        {
+            var corredor = new Corredor();
+            var aux = new Equipo();
+            string nombreEqui = "";
+
+            Console.WriteLine("Ingrese nombre del corredor: ");
+            corredor.Nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese apellido de corredor: ");
+            corredor.Apellido = Console.ReadLine();
+
+            corredor.Id = corredores.Count + 1;
+
+            do {
+                Console.WriteLine("Ingrese nombre del equipo");
+                nombreEqui = Console.ReadLine();
+
+                aux = BuscarEquipo(nombreEqui);
+
+                if (aux != null)
+                    corredor.RefEquipo = aux;
+            } while (aux == null);
+
+            corredores.Add(corredor);
+        }
+
+        private int BuscarLugarEtapa()
+        {
+            for (int i = 0; i < etapas.Length; i++) {
+                if (etapas[i] == null)
+                    return i;
+            }
+
+            return -1;
+        }
+
+        public void CrearEtapa()
+        {
+            Console.WriteLine(etapas.Count());
+
+            var etapa = new Etapa();
+            int i = 0;
+
+            Console.WriteLine("Ingrese fecha de la etapa: ");
+            etapa.FechaEtapa = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese kilometros de la etapa: ");
+            etapa.KilometroEtapa = int.Parse(Console.ReadLine());
+
+            i = BuscarLugarEtapa();
+
+            if (i != -1)
+                etapas[i] = etapa;
+            else
+                Console.WriteLine("Ya estan todas las etapas");
+        }
 
     }
 }
--- Carrera/Program.cs
+++ Carrera/Program.cs
@@ -10,6 +10,8 @@ namespace Carrera
     {
         static void Main(string[] args)
         {
+            Carrera carrera = new Carrera();
+            carrera.CrearEtapa();
         }
     }
 }
