daedb0700577aa3bd9a06a635f25cb339a449e27
Pelle <joaquinpellegrino@gmail.com>
2018-04-24 16:41:09 -0300

    Agrego el menu en el main y arreglo un pequeño error cuando se recorren las etapas de la carrarera

--- Carrera/Program.cs
+++ Carrera/Program.cs
@@ -10,6 +10,32 @@ namespace Carrera
     {
         static void Main(string[] args)
         {
+            var carrera = new Carrera();
+            char opc = ' ';
+
+            do {
+                Console.WriteLine("PARA SALIR PRESIONE N");
+
+                Console.WriteLine("1>Crear Equipo\n2>Crear correrdor\n3>Crear etapa");
+
+                opc = char.Parse(Console.ReadLine());
+                opc = char.ToUpper(opc);
+
+                switch (opc)
+                {
+                    case '1':
+                        carrera.CrearEquipo();
+                        break;
+                    case '2':
+                        carrera.CrearCorredor();
+                        break;
+                    case '3':
+                        carrera.CrearEtapa();
+                        break;
+                }
+            } while (opc != 'N');
+
+            carrera.InicioCarrera();
         }
     }
 }
